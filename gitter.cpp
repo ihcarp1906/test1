17250574e1550db023081e7bac17b11e57505ab3
Prachi <ihcarpshukla1906@gmail.com>
2019-09-02 08:54:07 +0530

    first commit

---
+++ gitter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+
+    cout<<"hello and welcome to the presentation";
+    cout<<"studying git and github here";
+
+    //our first version
+    return 0;
+}
